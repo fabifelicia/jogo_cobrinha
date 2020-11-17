60c26abf4f0e255299b0c51602404d861e22a081
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2020-11-16 21:30:02 -0300

    comidas e aumento da cobrinha

--- script.js
+++ script.js
@@ -55,7 +55,14 @@ function iniciarJogo() {
     if(direction === 'up') snakeY -= box
     if(direction === 'down') snakeY += box
 
-    snake.pop();
+    if (snakeX !== food.x || snakeY !== food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
+
+    
 
     let newHead = {
         x: snakeX,
@@ -65,4 +72,4 @@ function iniciarJogo() {
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 500);
